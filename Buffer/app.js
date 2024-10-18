a35ff0719364716962611faca5ad98e1e0f90616
harshitclub <harshitclub@gmail.com>
2024-10-18 15:52:09 +0530

    @buffer

---
+++ Buffer/app.js
@@ -0,0 +1,21 @@
+const { Buffer } = require("buffer");
+
+const memoryContainer = Buffer.alloc(4); //4 bytes | 32 bits
+
+console.log(memoryContainer);
+
+memoryContainer[1] = 1;
+
+console.log(memoryContainer[1]);
+
+memoryContainer[0] = 0xf4;
+memoryContainer[1] = 0x34;
+memoryContainer[2] = 0xb6;
+memoryContainer[3] = 0xff;
+
+console.log(memoryContainer);
+
+console.log(memoryContainer[0]);
+console.log(memoryContainer[1]);
+console.log(memoryContainer[2]);
+console.log(memoryContainer[3]);
