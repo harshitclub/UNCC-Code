c724fd4fcb37483ce844b46d067982f51b1267aa
harshitclub <harshitclub@gmail.com>
2024-10-18 16:09:51 +0530

    @buffer

--- Buffer/app.js
+++ Buffer/app.js
@@ -13,7 +13,7 @@ memoryContainer[1] = 0x34;
 memoryContainer[2] = 0xb6;
 memoryContainer[3] = 0xff;
 
-console.log(memoryContainer);
+console.log(memoryContainer.toString("utf-8"));
 
 console.log(memoryContainer[0]);
 console.log(memoryContainer[1]);
